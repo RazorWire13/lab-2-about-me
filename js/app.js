50b9794f2e9502354431350e207e79951da2d480
RazorWire13 <baneblade13@gmail.com>
2018-08-09 09:57:09 -0700

    Updated comments, CSS styles and added HTML lists

--- js/app.js
+++ js/app.js
@@ -1,17 +1,21 @@
 'use strict';
 
-// Get the user's name
+// Lab 2 Get the user's name
+
 var userName = prompt('Heya! What is your name?');
 console.log('User was asked their name and responded with ' + userName);
 
-alert('Glad you could join me, ' + userName + '! Let\'s play a little game of five yes/no questions to see how well you know Dave. Please answer with y/n or yes/no responses.');
+alert('Glad you could join me, ' + userName + '! Let\'s play a little game of five questions to see how well you know me. Please answer with y/n or yes/no responses.');
 
-// User score ticker variable
+
+// Lab 3 User score ticker variable
 
 var totalScore = 0;
 
-// Ask them five y/n questions
 
+// Lab 2 Ask user five y/n questions
+
+// Question 1
 var answerOne = prompt('Ok, this one is easy - do I have any siblings?').toLowerCase();
 console.log('User responded to first question with ' + answerOne);
 
@@ -22,6 +26,7 @@ if (answerOne === 'y' || answerOne === 'yes') {
   alert('No. If you got this wrong then this may be a painful quiz.');
 }
 
+// Question 2
 var answerTwo = prompt('Do I have any technical certifications?').toLowerCase();
 console.log('User responded to second question with ' + answerTwo);
 
@@ -32,6 +37,7 @@ if (answerTwo === 'n' || answerTwo === 'no') {
   alert('Sorry, no. I do not have any yet.');
 }
 
+// Question 3
 var answerThree = prompt('Did I serve in the Air Force?').toLowerCase();
 console.log('User responded to third question with ' + answerThree);
 
@@ -39,9 +45,10 @@ if (answerThree === 'n' || answerThree === 'no') {
   alert('That\'s right! I served 9 years in the Army, not the "Chair Force"');
   totalScore++;
 } else {
-  alert('Ouch - thought you knew me. I was a soldier for 9 years!');
+  alert('Ouch - thought you knew me. I was actually a soldier for 9 years.');
 }
 
+// Question 4
 var answerFour = prompt('Ok, do I have a love for fried pickles?').toLowerCase();
 console.log('User responded to fourth question with ' + answerFour);
 
@@ -52,6 +59,7 @@ if (answerFour === 'y' || answerFour === 'yes') {
   alert('My apologies, we clearly have not hung out as you would definietly know this about me if we had.');
 }
 
+// Question 5
 var answerFive = prompt('This one might be tough, but do I have a degree in political science?').toLowerCase();
 console.log('User responded to fifth question with ' + answerFive);
 
@@ -62,7 +70,9 @@ if (answerFive === 'y' || answerFive === 'yes') {
   alert('It\'s true. I actually do have a BS in Poly Sci. Should have done engineering in hindsight, but whatever.');
 }
 
+
 // Lab 3 - Question 6 My favorite number
+
 var faveNumber = '13';
 var i = 0;
 do {
@@ -78,7 +88,9 @@ do {
   }
 } while (i < 5);
 
-// // Lab 3 - Question 7 Guess where I have lived
+
+// Lab 3 - Question 7 Guess where I have lived
+
 var placesLived = ['florida', 'germany', 'ethiopia', 'afghanistan', 'maryland'];
 var ii = 0;
 do {
@@ -107,7 +119,9 @@ do {
     alert('Nope. Try Again');
     ii++;
   }
-} while ( ii < 6);
+} while (ii < 6);
 
-// User's total score
+
+// Lab 3 User's total score
+
 alert('You got ' + totalScore + ' out of a possible 7 correct. Thank you for playing, ' + userName + '!');
